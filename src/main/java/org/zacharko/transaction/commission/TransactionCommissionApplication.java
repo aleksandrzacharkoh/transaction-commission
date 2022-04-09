0bc151ac20e6cc1e02b71a3ccd3213bd1001ab36
Aleksandr Zacharko <aleksandr.zacharko@homeadvisor.com>
2022-04-09 11:11:58 +0200

    Initial commit

---
+++ src/main/java/org/zacharko/transaction/commission/TransactionCommissionApplication.java
@@ -0,0 +1,13 @@
+package org.zacharko.transaction.commission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionCommissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionCommissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zacharko/transaction/commission/TransactionCommissionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zacharko.transaction.commission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCommissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
